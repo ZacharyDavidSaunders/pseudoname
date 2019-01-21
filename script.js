9241552a69f90fe4f9ac166cab0289a1a144a4f1
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2019-01-21 02:48:12 -0800

    Completed MVP, fixed typos, tweaked, CSS and added donation button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var corsProxy = "https://cors-anywhere.herokuapp.com/";
+
 function createAlias(){
   hideAllResponses();
   if(verifyInput()){
@@ -6,25 +8,32 @@ function createAlias(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
-             alert(this.responseText);
+             if(this.responseText.includes("Alias created")){
+               showResponse("Success!","Your alias has been created! <br><br>You may now use "+aliasInput.value+"@pseudoname.io, all emails sent to that address will be forwarded to "+realEmailInput.value+".<br><br><i>If you like Pseudoname and would like to keep the service free, please consider making a small donation via the button below:</i>", 1);
+             }else if(this.responseText.includes("You can only define the same source once per domain")){
+               showResponse("Error: Alias Unavailable","That alias has already been taken by someone else. Please choose a different alias and try again.", 2);
+             }else{
+               showResponse("Error: Unknown API Response", "An unexpected error occured. Please try again later. If this problem persists, please <a href=\"contact.html\">get in touch with us.</a>", 2);
+               console.log(this.responseText);
+             }
+         }else if (this.readyState == 4){
+           showResponse("Error: Something's Wrong", "Something went wrong when creating your alias. Please try again later. If this problem persists, please <a href=\"contact.html\">get in touch with us.</a>", 2);
          }else{
-            showResponse("Error: Something's wrong", "Something went wrong when creating your alias. Please try again later", false);
+           showResponse("Loading", "Please wait...", 3);
          }
     };
-    xhttp.open("POST", "https://forwardmx.io/api/alias/create", true);
-    xhttp.setRequestHeader("key","S7CIbakmt8jXSSz0Svjm1End67Bwh828sUs");
-    xhttp.setRequestHeader("domain","pseudoname.io");
-    xhttp.setRequestHeader("alias",aliasInput.value);
-    xhttp.setRequestHeader("destination",realEmailInput.value);
+    xhttp.open("POST", corsProxy.concat("https://forwardmx.io/api/alias/create?"+
+    "&key=S7CIbakmt8jXSSz0Svjm1End67Bwh828sUs"+
+    "&domain=pseudoname.io"+
+    "&destination="+realEmailInput.value+
+    "&alias="+aliasInput.value), true);
     xhttp.send();
   }
 }
 
 function deleteAlias(){
   hideAllResponses();
-  if(verifyInput()){
-
-  }
+  showResponse("How To Delete An Alias:", "In order to prevent abuse, we have temporarily disabled automatic alias deletion. If you'd like to remove an alias, please <a href=\"contact.html\">contact us</a> and we will happily assist you.", 3);
 }
 
 function verifyInput(){
@@ -38,27 +47,30 @@ function verifyInput(){
       if(!emailVerificationRegex.test(aliasInput.value.toLowerCase()) && !(aliasInput.value.includes('@'))){
         return true;
       }else{
-        showResponse("Error: Invalid Alias Email", "The alias email that you entered is invalid, you don't need to include the '@' character, nor any characters after it. Please try again.", false);
+        showResponse("Error: Invalid Alias Email", "The alias email that you entered is invalid, you don't need to include the '@' character, nor any characters after it. Please try again.", 2);
         return false;
       }
     }else{
-      showResponse("Error: Invalid Real Email", "The real email that you entered is invalid. Please try again.", false);
+      showResponse("Error: Invalid Real Email", "The real email that you entered is invalid. Please try again.", 2);
       return false;
     }
   }else{
-    showResponse("Error:", "Both a real email and an email alias must be entered to proceed.", false);
+    showResponse("Error:", "Both a real email and an email alias must be entered to proceed.", 2);
     return false;
   }
   return true;
 }
 
-function showResponse(responseHeader,responseText,positiveBoolean){
+function showResponse(responseHeader,responseText,statusCode){
   var responseElement = document.getElementById("response");
 
-  if(positiveBoolean){
-    responseElement.innerHTML = "<strong class=positiveResponse>"+responseHeader+"</strong><br><br>"+responseText;
-  }else{
+  if(statusCode == 1){
+    responseElement.innerHTML = "<strong class=positiveResponse>"+responseHeader+"</strong><br><br>"+responseText+"<br><br>"+
+    "<a href='https://ko-fi.com/M4M4P3CB' target='_blank'><img height='36' style='border:0px;height:36px;' src='imgs/SupportButton.jpg' border='0' alt='Support Pseudoname'/></a>";
+  }else if(statusCode == 2){
     responseElement.innerHTML = "<strong class=negativeResponse>"+responseHeader+"</strong><br><br>"+responseText;
+  }else{
+    responseElement.innerHTML = "<strong class=neutralResponse>"+responseHeader+"</strong><br><br>"+responseText;
   }
   responseElement.style.display = "block";
   responseElement.style.visibility = "visible";
